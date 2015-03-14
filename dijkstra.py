99be58f6042fc384e15526a9992da0a2afb15204
nishidy <nishidyatutokyo@gmail.com>
2015-03-15 08:24:44 +0900

    Only have some code cleanup.

--- dijkstra.py
+++ dijkstra.py
@@ -14,7 +14,7 @@ class Node:
 		self.fn=-1 # from node (for debug)
 		self.fix=False
 
-class Node_in_queue:
+class Node_in_q:
 	def __init__(self,n,co):
 		self.n=n
 		self.co=co
@@ -54,10 +54,13 @@ go=int(line[1])
 if st not in edges or go not in edges:
 	sys.exit(1)
 
-qu=Queue.PriorityQueue() # Node_in_queue obj
+qu=Queue.PriorityQueue() # Node_in_q obj
 
 nodes[st].co=0
-qu.put(Node_in_queue(st,0))
+qu.put(Node_in_q(st,0))
+
+df=False
+if len(sys.argv)>1 and int(sys.argv[1])==1: df=True
 
 print "[Shortest path search by dijkstra algorithm]\n"
 
@@ -75,8 +78,8 @@ while not qu.empty():
 			return reduce(lambda x,y: str(x)+"->"+str(y),nl[1:],nl[0])
 		else:
 			return str(nl[0])
-	if len(sys.argv)>1 and int(sys.argv[1])==1:
-		print "%s (%d)"%(debug(),frn.co)
+
+	if df: print "%s (%d)"%(debug(),frn.co)
 
 	nodes[frn.n].fix=True
 	if frn.n==go: break
@@ -86,7 +89,7 @@ while not qu.empty():
 		if frn.co+ed.co<nodes[ed.to].co:
 			nodes[ed.to].co=frn.co+ed.co
 			nodes[ed.to].fn=frn.n
-			qu.put(Node_in_queue(ed.to,frn.co+ed.co))
+			qu.put(Node_in_q(ed.to,frn.co+ed.co))
 
 print "Min cost is %d (%s)."%(frn.co,debug())
 
