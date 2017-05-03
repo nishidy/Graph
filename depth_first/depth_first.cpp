c041ded9caaf265a4f4ffd15977ca5acf06b18d8
nishidy <nishidyatutokyo@gmail.com>
2017-05-03 20:51:12 +0900

    Updated depth_first in cpp to ver 2.

--- depth_first/depth_first.cpp
+++ depth_first/depth_first.cpp
@@ -1,153 +1,126 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
-using namespace std;
-typedef vector<int>::iterator itevi;
-typedef vector<pair<int,int> >::iterator itevpii;
+#include <iterator>
 
-int DEBUG=0;
+using namespace std;
+
+typedef unsigned int UI;
+typedef UI ID;
+typedef UI COST;
 
 class Node{
-	public:
-	static vector<Node> all_nodes;
-	int id;
-	//int cost;
-	Node* min_from_node;
-	bool visited;
-	vector<pair<int,int> > to_node_cost;
-	Node(){};
-	Node(int id){
-		this->id=id;
-		//this->cost=0;
-		this->visited=false;
-		this->min_from_node=NULL;
-	};
+
+    public:
+
+    ID id;
+    bool depth_visited;
+    unordered_map<ID,COST> to_nodes;
+
+    Node(){}
+    Node(ID id):id(id){}
+
 };
 
-vector<Node> Node::all_nodes;
-map<int,map<int,int> > memo;
+class Graph{
+    ID start, goal;
+    unordered_map<ID,Node*> nodes;
+    ID max_id;
 
-typedef vector<Node>::iterator itevnode;
+    public:
 
-Node* get_node(int id){
-	Node* node=NULL;
-	itevnode ite=Node::all_nodes.begin();
-	while(ite!=Node::all_nodes.end()){
-		if(ite->id==id){
-			node=&(*ite);
-			break;
-		}
-		ite++;
-	}
-	return node;
+    Graph(){}
+    Graph(ID s, ID g):start(s), goal(g){}
+
+    void create_node(ID id){
+        if(nodes.find(id)==nodes.end()){
+            nodes[max_id] = new Node(max_id);
+            max_id++;
+        }
+    }
+
+    void add_to_node(ID from, ID to, COST cost){
+        //cout<<from<<to<<cost<<endl;
+        Node* node = nodes[from];
+        node->depth_visited = false;
+        node->to_nodes[to] = cost;
+    }
+
+    void search(){
+        vector<ID> path, best;
+        cout << start << " ---> " << goal << endl;
+        cout << "cost : " << do_search(start,0,1<<31,path,best) << endl;;
+        cout << "path : ";
+        for(auto node : best){
+            cout << node;
+            if(node!=best.back()){
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+    }
+
+    // path is copied variable using as stack
+    // while best is reference pointer
+    COST do_search(ID id, COST cost, COST min, vector<ID> path, vector<ID>& best){
+        path.push_back(id);
+
+        if(id==goal){
+            min = min<cost?min:cost;
+            if(min==cost){
+                best.clear();
+                copy(path.begin(),path.end(),back_inserter(best));
+            }
+
+            /* debug
+            for(auto node: path){
+                cout << node;
+                if(node!=path.back()){
+                    cout << " -> ";
+                }
+            }
+            cout << " : " << min << endl;
+            */
+
+            return min;
+        }
+
+        nodes[id]->depth_visited = true;
+        for(auto next: nodes[id]->to_nodes){
+            ID   next_id   = next.first;
+            COST next_cost = cost+next.second;
+            //cout<<id<<"->"<<next_id<<":"<<next_cost<<endl;
+            if(nodes[next_id]->depth_visited){
+                continue;
+            }
+            min = do_search(next_id,next_cost,min,path,best);
+        }
+        nodes[id]->depth_visited = false;
+        return min;
+    }
+};
+
+
+int main(){
+
+    UI num_edges, num_nodes;
+    UI start,goal;
+    cin>>num_nodes>>num_edges;
+    cin>>start>>goal;
+
+    Graph graph = Graph(start,goal);
+
+    UI from,to,cost;
+    for(UI i=0;i<num_edges;i++){
+        cin>>from>>to>>cost;
+        graph.create_node(from);
+        graph.create_node(to);
+        graph.add_to_node(from,to,cost);
+    }
+
+    graph.search();
+
+    return 0;
 }
-
-int search(int cost,int min_cost, Node* node, Node* goal_node, int depth){
-
-	if(DEBUG==1){
-		for(int i=0;i<depth;i++) printf("->");
-		printf("%d(%d)",node->id,cost);
-	}
-
-	int return_cost;
-	node->visited=true;
-	if(node->id==goal_node->id){
-		if(DEBUG==1) printf("//\n");
-		return_cost=cost;
-
-	}else{
-		if(DEBUG==1) printf("\n");
-
-		int tmp_min_cost,next_cost;
-		Node* next_node;
-		itevpii ite=node->to_node_cost.begin();
-		while(ite!=node->to_node_cost.end()){
-			next_node=get_node(ite->first);
-			next_cost=cost+ite->second;
-			if(next_node->visited==false){
-				if(min_cost<next_cost){
-
-				}else{
-					if(memo.find(next_node->id)!=memo.end() &&\
-					   memo[next_node->id].find(next_cost)!=memo[next_node->id].end()){
-						tmp_min_cost=memo[next_node->id][next_cost];
-					}else{
-						tmp_min_cost=search(next_cost,min_cost,next_node,goal_node,depth+1);
-						memo[next_node->id][next_cost]=tmp_min_cost;
-					}
-					if(min_cost>tmp_min_cost){
-						next_node->min_from_node=node;
-						min_cost=tmp_min_cost;
-					}
-				}
-			}
-			ite++;
-		}
-		return_cost=min_cost;
-
-	}
-
-	node->visited=false;
-	return return_cost;
-}
-
-void search_path(Node* node,vector<int> min_path_nodes){
-	min_path_nodes.push_back(node->id);
-	if(node->min_from_node==NULL){
-		reverse(min_path_nodes.begin(),min_path_nodes.end());
-		itevi ite=min_path_nodes.begin();
-		printf("(");
-		while(ite!=min_path_nodes.end()){
-			if(ite!=min_path_nodes.begin()) printf("->");
-			printf("%d",*ite);
-			ite++;
-		}
-		printf(").\n");
-		return;
-	}
-	search_path(node->min_from_node,min_path_nodes);
-}
-
-int main(int argc, char* argv[]){
-	printf("[Shortest path search by depth first algorithm]\n\n");
-	if(argc==2&&1==atoi(argv[1])) DEBUG=1;
-
-	int num_edges, num_nodes;
-	int from,to,cost;
-	int start,goal;
-	Node* node;
-
-	cin>>num_nodes>>num_edges;
-	cin>>start>>goal;
-
-	for(int i=0;i<num_edges;i++){
-		cin>>from>>to>>cost;
-		node=get_node(from);
-		if(node==NULL){
-			Node new_node(from);
-			Node::all_nodes.push_back(new_node);
-			node=&(Node::all_nodes.back());
-		}
-		node->to_node_cost.push_back(pair<int,int>(to,cost));
-
-		node=get_node(to);
-		if(node==NULL){
-			Node new_node(to);
-			Node::all_nodes.push_back(new_node);
-			node=&(Node::all_nodes.back());
-		}
-		node->to_node_cost.push_back(pair<int,int>(from,cost));
-	}
-
-	from=start;
-
-	int min_cost=1<<20;
-	printf("Min cost is %d ",search(0,min_cost,get_node(from),get_node(goal),0));
-	vector<int> min_path_nodes;
-	search_path(get_node(goal),min_path_nodes);
-
-	return 0;
-}
-
 
