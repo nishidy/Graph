a52518203ad15cfc36ec26bc78b43bd698d0767e
nishidy <nishidyatutokyo@gmail.com>
2017-05-04 08:22:30 +0900

    Added depth_first in python.

--- depth_first/README.md
+++ depth_first/README.md
@@ -4,3 +4,10 @@ $ ./tools/generate_graph_v2 20 100 0 19 10000 | depth_first/depth_first
 cost : 12873
 path : 0 -> 17 -> 1 -> 13 -> 19
 ```
+
+```
+$ ./tools/generate_graph_v2 20 100 0 19 10000 | python depth_first/depth_first.py 
+0  ---> 19
+cost :  12873
+path :  0 -> 17 -> 1 -> 13 -> 19
+```
---
+++ depth_first/depth_first.py
@@ -0,0 +1,59 @@
+import sys
+from functools import reduce
+
+class Node:
+    def __init__(self,node_id):
+        self.node_id=node_id
+        self.to_nodes = {}
+        self.visited = False
+
+class Graph:
+    def __init__(self,start,goal):
+        self.start=start
+        self.goal=goal
+        self.nodes = {}
+
+    def create_node(self,node_id):
+        if node_id not in self.nodes:
+            self.nodes[node_id] = Node(node_id)
+
+    def add_to_node(self,fr,to,cost):
+        self.nodes[fr].to_nodes[to] = cost
+
+    def search(self):
+        print(self.start," --->", self.goal)
+        best = []
+        print("cost : ",self.do_search(start,0,1<<31,[],best))
+        print("path : ",reduce(lambda a,b:str(a)+" -> "+str(b),best))
+
+    def do_search(self,node_id,cost,min_cost,path,best):
+        path.append(node_id)
+        if node_id == self.goal:
+            if cost<min_cost:
+                min_cost=cost
+            if cost==min_cost:
+                best[:]=path.copy()
+            return min_cost
+        #print(path)
+        self.nodes[node_id].visited=True
+        for next_ in self.nodes[node_id].to_nodes.items():
+            next_id   = next_[0]
+            next_cost = cost+next_[1]
+            if self.nodes[next_id].visited:
+                continue
+            min_cost = self.do_search(next_id,next_cost,min_cost,path.copy(),best)
+        self.nodes[node_id].visited=False
+        return min_cost
+
+num_nodes,num_edges=map(lambda x:int(x),input().split())
+start,goal=map(lambda x:int(x),input().split())
+
+graph=Graph(start,goal)
+for n in range(num_edges):
+    fr,to,cost=map(lambda x:int(x),input().split())
+    graph.create_node(fr)
+    graph.create_node(to)
+    graph.add_to_node(fr,to,cost)
+
+graph.search()
+
